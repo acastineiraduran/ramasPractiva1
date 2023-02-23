dc44a7ba4226310c75776d1a2f234ae6da5978b0
acastineiraduran <acastineiraduran@danielcastelao.org>
2023-02-23 22:13:59 +0100

    carpeta docs

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+/**
+ * clase main mierdas
+ *
+ * @author casty
+ * @version 1.0
+ */
 public class Main {
+    /**
+     * metodo main
+     * @param args
+     */
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
         System.out.println("Segundo commit en la main");
         System.out.println("otra implementacion");
