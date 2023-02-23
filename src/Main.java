c6658d0457f2bb0f451516f988f9954c2a97f700
acastineiraduran <acastineiraduran@danielcastelao.org>
2023-02-23 21:59:12 +0100

    C

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Segundo commit en la main");
+        System.out.println("otra implementacion");
     }
 }
